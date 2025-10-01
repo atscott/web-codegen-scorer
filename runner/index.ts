e0aa3947ba351442c493d2d82926a8d66be0209f
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-01 21:16:53 +0200

    refactor: expose more types
    Exposes more types necessary to implement a custom gateway.

--- runner/index.ts
+++ runner/index.ts
@@ -1,6 +1,7 @@
 export * from './shared-interfaces.js';
 export * from './configuration/environment-config.js';
 export * from './orchestration/gateway.js';
+export * from './orchestration/gateways/local_gateway.js';
 export {
   type RemoteEnvironmentConfig,
   RemoteEnvironment,
@@ -18,11 +19,21 @@ export {
 } from './workers/builder/builder-types.js';
 export {type UserJourneysResult} from './orchestration/user-journeys.js';
 export {type AutoRateResult} from './ratings/autoraters/auto-rate-shared.js';
-export {type McpServerOptions} from './codegen/llm-runner.js';
 export {DEFAULT_MODEL_NAME, REPORT_VERSION} from './configuration/constants.js';
 export {generateCodeAndAssess} from './orchestration/generate.js';
 export {groupSimilarReports} from './orchestration/grouping.js';
-export {type LlmRunner} from './codegen/llm-runner.js';
+export {
+  type LlmRunner,
+  type LlmGenerateFilesContext,
+  type LlmGenerateFilesRequestOptions,
+  type LlmGenerateTextRequestOptions,
+  type LlmConstrainedOutputGenerateRequestOptions,
+  type LlmConstrainedOutputGenerateResponse,
+  type LlmGenerateFilesResponse,
+  type LlmGenerateTextResponse,
+  type McpServerOptions,
+  type PromptDataMessage,
+} from './codegen/llm-runner.js';
 export {GenkitRunner} from './codegen/genkit/genkit-runner.js';
 export {GeminiCliRunner} from './codegen/gemini-cli-runner.js';
 export {getRunnerByName, type RunnerName} from './codegen/runner-creation.js';
@@ -30,3 +41,7 @@ export {getEnvironmentByPath} from './configuration/environment-resolution.js';
 export {type Environment} from './configuration/environment.js';
 export {autoRateFiles} from './ratings/autoraters/rate-files.js';
 export {fetchReportsFromDisk} from './reporting/report-local-disk.js';
+export {type ProgressLogger, type ProgressType} from './progress/progress-logger.js';
+export {DynamicProgressLogger} from './progress/dynamic-progress-logger.js';
+export {NoopProgressLogger} from './progress/noop-progress-logger.js';
+export {TextProgressLogger} from './progress/text-progress-logger.js';
